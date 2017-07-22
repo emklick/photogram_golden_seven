3159403b43594e75d552729dd2e6a93b48d0396f
Em Klick <eklick@chicagobooth.edu>
2017-07-22 23:26:32 +0000

    4

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,53 +1,53 @@
-class PicturesController < ApplicationController
-  def new_form
-    render("pic_templates/new_form.html.erb")
-  end
-
-  def create_row
-    p = Photo.new
-
-    p.source = params[:the_source]
-    p.caption = params[:the_caption]
-
-    p.save
-
-    redirect_to("/photos")
-  end
-
-  def index
-    @list_of_photos = Photo.order(:created_at => :desc)
-
-    render("pic_templates/index.html.erb")
-  end
-
-  def show
-    @photo = Photo.find(params[:an_id])
-
-    render("pic_templates/show.html.erb")
-  end
-
-  def edit_form
-    @photo = Photo.find(params[:some_id])
-
-    render("pic_templates/edit_form.html.erb")
-  end
-
-  def update_row
-    p = Photo.find(params[:the_id])
-
-    p.source = params[:the_source]
-    p.caption = params[:the_caption]
-
-    p.save
-
-    redirect_to("/photos/#{p.id}")
-  end
-
-  def destroy_row
-    p = Photo.find(params[:toast_id])
-
-    p.destroy
-
-    redirect_to("/photos")
-  end
-end
+class PicturesController <ApplicationController
+    
+    def new_form
+        
+        
+        
+        render ("pic_templates/new_form.html.erb")
+    end    
+    
+    def create_row
+        
+        
+        
+        render ("pic_templates/create_row.html.erb")
+    end 
+    
+    def index
+        
+        
+        
+        render ("pic_templates/index_template.html.erb")
+    end  
+    
+    def show
+        
+        
+        
+        render ("pic_templates/show.html.erb")
+    end  
+    
+    def edit_form
+        
+        
+        
+        render ("pic_templates/edit_form.html.erb")
+    end  
+    
+    def update_row
+        
+        
+        
+        render ("pic_templates/update_row.html.erb")
+    end  
+    
+    def destroy_row
+        
+        
+        
+        render ("pic_templates/destroy_row.html.erb")
+    end  
+    
+    
+end    
--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,47 @@ Rails.application.routes.draw do
 
   # CREATE
   get("/photos/new", { :controller => "pictures", :action => "new_form" })
+  
   get("/create_photo", { :controller => "pictures", :action => "create_row" })
+
+
 
   # READ
   get("/photos", { :controller => "pictures", :action => "index" })
-  get("/photos/:an_id", { :controller => "pictures", :action => "show" })
+  
+  get("/photos/:id_number", { :controller => "pictures", :action => "show" })
+
+
 
   # UPDATE
-  get("/photos/:some_id/edit", { :controller => "pictures", :action => "edit_form" })
-  get("/update_photo/:the_id", { :controller => "pictures", :action => "update_row" })
+  get("/photos/:photo_id_id/edit", { :controller => "pictures", :action => "edit_form" })
+  
+  get("/update_photo/:id", { :controller => "pictures", :action => "update_row" })
+
+
 
   # DELETE
-  get("/delete_photo/:toast_id", { :controller => "pictures", :action => "destroy_row" })
+  get("/delete_photo/:adios_id", { :controller => "pictures", :action => "destroy_row" })
 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   mount WebGit::Engine, at: "/rails/git"
+
+
+
+
+
 end
